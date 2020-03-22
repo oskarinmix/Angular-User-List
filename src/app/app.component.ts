873863d3bf7a97a3b06213d8ef2657c9c3aec8c8
Oscar Alberto Ceballos Contreras <oscaralbertoceballos@gmail.com>
2020-03-22 10:12:35 -0400

    added shuffle users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import  users from './users';
+import { Component, OnInit } from "@angular/core";
+import users from "./users";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  title = "Labs XD Innovation Test";
+  title = 'Labs XD Innovation Test';
   users = null;
 
+  shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
   ngOnInit() {
+    const shuffleUsers = this.shuffle(users);
+
     setTimeout(() => {
-      this.users = users;
+      this.users = shuffleUsers.slice(0, 10);
     }, 2000);
   }
 }
--- src/app/users.js
+++ src/app/users.js
@@ -4,7 +4,7 @@ const users = [
     name: "Leanne Graham",
     username: "Bret",
     age: 28,
-    sex: 'women',
+    sex: "women",
     email: "Sincere@april.biz",
     address: {
       street: "Kulas Light",
@@ -28,8 +28,8 @@ const users = [
     id: 2,
     name: "Ervin Howell",
     username: "Antonette",
-    age: 38 ,
-    sex: 'men',
+    age: 38,
+    sex: "men",
     email: "Shanna@melissa.tv",
     address: {
       street: "Victor Plains",
@@ -54,7 +54,7 @@ const users = [
     name: "Clementine Bauch",
     username: "Samantha",
     age: 24,
-    sex:'women',
+    sex: "women",
     email: "Nathan@yesenia.net",
     address: {
       street: "Douglas Extension",
@@ -79,7 +79,7 @@ const users = [
     name: "Patricia Lebsack",
     username: "Karianne",
     age: 22,
-    sex:'women',
+    sex: "women",
     email: "Julianne.OConner@kory.org",
     address: {
       street: "Hoeger Mall",
@@ -104,7 +104,7 @@ const users = [
     name: "Chelsey Dietrich",
     username: "Kamren",
     age: 27,
-    sex:'women',
+    sex: "women",
     email: "Lucio_Hettinger@annie.ca",
     address: {
       street: "Skiles Walks",
@@ -129,7 +129,7 @@ const users = [
     name: "Dennis Schulist",
     username: "Leopoldo_Corkery",
     age: 32,
-    sex:'men',
+    sex: "men",
     email: "Karley_Dach@jasper.info",
     address: {
       street: "Norberto Crossing",
@@ -154,7 +154,7 @@ const users = [
     name: "Kurtis Weissnat",
     username: "Elwyn.Skiles",
     age: 40,
-    sex:'men',
+    sex: "men",
     email: "Telly.Hoeger@billy.biz",
     address: {
       street: "Rex Trail",
@@ -179,7 +179,7 @@ const users = [
     name: "Nicholas Runolfsdottir V",
     username: "Maxime_Nienow",
     age: 19,
-    sex:'men',
+    sex: "men",
     email: "Sherwood@rosamond.me",
     address: {
       street: "Ellsworth Summit",
@@ -204,7 +204,7 @@ const users = [
     name: "Glenna Reichert",
     username: "Delphine",
     age: 24,
-    sex:'women',
+    sex: "women",
     email: "Chaim_McDermott@dana.io",
     address: {
       street: "Dayna Park",
@@ -229,7 +229,7 @@ const users = [
     name: "Sandra DuBuque",
     username: "Moriah.Stanton",
     age: 41,
-    sex:'women',
+    sex: "women",
     email: "Rey.Padberg@karina.biz",
     address: {
       street: "Kattie Turnpike",
@@ -248,8 +248,270 @@ const users = [
       catchPhrase: "Centralized empowering task-force",
       bs: "target end-to-end models"
     }
+  },
+  {
+    id: 11,
+    name: "Karen Redman",
+    username: "kman",
+    age: 42,
+    sex: "women",
+    email: "Sincere@april.biz",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496"
+      }
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Reddis-Credd",
+      catchPhrase:
+        "Your childhood teacher did not wrong you when they taught you that there should be three",
+      bs: "harness real-time e-markets"
+    }
+  },
+  {
+    id: 12,
+    name: "Oscar Farewll",
+    username: "Antonette",
+    age: 32,
+    sex: "men",
+    email: "Shanna@melissa.tv",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Wisokyburgh",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618"
+      }
+    },
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+    company: {
+      name: "scalable-chains",
+      catchPhrase:
+        "The model regarding paragraph length that your teacher undoubtedly taught you involves a topic sentence",
+      bs: "synergize scalable supply-chains"
+    }
+  },
+  {
+    id: 13,
+    name: "Laura Derek",
+    username: "Samantha",
+    age: 24,
+    sex: "women",
+    email: "Nathan@yesenia.net",
+    address: {
+      street: "Douglas Extension",
+      suite: "Suite 847",
+      city: "McKenziehaven",
+      zipcode: "59590-4157",
+      geo: {
+        lat: "-68.6102",
+        lng: "-47.0653"
+      }
+    },
+    phone: "1-463-123-4447",
+    website: "ramiro.info",
+    company: {
+      name: "ramiro-enable",
+      catchPhrase:
+        "You can see from this example how a topic is introduced, supported, and then brought to its natural conclusion",
+      bs: "e-enable strategic applications"
+    }
+  },
+  {
+    id: 14,
+    name: "Andrea Joask",
+    username: "Karianne",
+    age: 19,
+    sex: "women",
+    email: "Julianne.OConner@kory.org",
+    address: {
+      street: "Hoeger Mall",
+      suite: "Apt. 692",
+      city: "South Elvis",
+      zipcode: "53919-4257",
+      geo: {
+        lat: "29.4572",
+        lng: "-164.2990"
+      }
+    },
+    phone: "493-170-9623 x156",
+    website: "kale.biz",
+    company: {
+      name: "edge-transition",
+      catchPhrase:
+        "You can only feel the beauty. “The best and most beautiful things in the world cannot be seen or even touched",
+      bs: "transition cutting-edge web services"
+    }
+  },
+  {
+    id: 15,
+    name: "Danniana Pettersen",
+    username: "Kamren",
+    age: 32,
+    sex: "women",
+    email: "Lucio_Hettinger@annie.ca",
+    address: {
+      street: "Skiles Walks",
+      suite: "Suite 351",
+      city: "Roscoeview",
+      zipcode: "33263",
+      geo: {
+        lat: "-31.8129",
+        lng: "62.5342"
+      }
+    },
+    phone: "(254)954-1289",
+    website: "demarco.info",
+    company: {
+      name: "systems revolutionize",
+      catchPhrase: "Learn to say 'No' ...Be a Creator, not Consumer.",
+      bs: "revolutionize end-to-end systems"
+    }
+  },
+  {
+    id: 16,
+    name: "Kevin Williams",
+    username: "Leopoldo_Corkery",
+    age: 26,
+    sex: "men",
+    email: "Karley_Dach@jasper.info",
+    address: {
+      street: "Norberto Crossing",
+      suite: "Apt. 950",
+      city: "South Christy",
+      zipcode: "23505-1337",
+      geo: {
+        lat: "-71.4197",
+        lng: "71.7478"
+      }
+    },
+    phone: "1-477-935-8478 x6430",
+    website: "ola.org",
+    company: {
+      name: "applications-innovative",
+      catchPhrase:
+        "In love we discover the good qualities of our loved one which are hidden from other people",
+      bs: "e-enable innovative applications"
+    }
+  },
+  {
+    id: 17,
+    name: "Hug Updike",
+    username: "Elwyn.Skiles",
+    age: 40,
+    sex: "men",
+    email: "Telly.Hoeger@billy.biz",
+    address: {
+      street: "Rex Trail",
+      suite: "Suite 280",
+      city: "Howemouth",
+      zipcode: "58804-1099",
+      geo: {
+        lat: "24.8918",
+        lng: "21.8984"
+      }
+    },
+    phone: "210.067.6132",
+    website: "elvis.io",
+    company: {
+      name: "Several Force",
+      catchPhrase:
+        "Many waters cannot quench love, neither can the floods drown it. If a man were to give all his wealth for love, it would be utterly scorned. ",
+      bs: "generate enterprise e-tailers"
+    }
+  },
+  {
+    id: 18,
+    name: "John Keats",
+    username: "Maxime_Nienow",
+    age: 23,
+    sex: "men",
+    email: "Sherwood@rosamond.me",
+    address: {
+      street: "Ellsworth Summit",
+      suite: "Suite 729",
+      city: "Aliyaview",
+      zipcode: "45169",
+      geo: {
+        lat: "-14.3990",
+        lng: "-120.7677"
+      }
+    },
+    phone: "586.493.6943 x140",
+    website: "jacynthe.com",
+    company: {
+      name: "Secondary Concept",
+      catchPhrase:
+        "When you love someone, all your saved-up wishes start coming out. ",
+      bs: "e-enable extensible e-tailers"
+    }
+  },
+  {
+    id: 19,
+    name: "Zelda Fitzgerald",
+    username: "Delphine",
+    age: 34,
+    sex: "women",
+    email: "Chaim_McDermott@dana.io",
+    address: {
+      street: "Dayna Park",
+      suite: "Suite 449",
+      city: "Bartholomebury",
+      zipcode: "76495-3109",
+      geo: {
+        lat: "24.6463",
+        lng: "-168.8889"
+      }
+    },
+    phone: "(775)976-6794 x41206",
+    website: "conrad.com",
+    company: {
+      name: "aggregate technologies timeSons",
+      catchPhrase:
+        "Nobody has ever measured, not even poets, how much the heart can hold. ",
+      bs: "aggregate real-time technologies"
+    }
+  },
+  {
+    id: 20,
+    name: "Julia Moore",
+    username: "Moriah.Stanton",
+    age: 30,
+    sex: "women",
+    email: "Rey.Padberg@karina.biz",
+    address: {
+      street: "Kattie Turnpike",
+      suite: "Suite 198",
+      city: "Lebsackbury",
+      zipcode: "31428-2261",
+      geo: {
+        lat: "-38.2386",
+        lng: "57.2232"
+      }
+    },
+    phone: "024-648-3804",
+    website: "ambrose.net",
+    company: {
+      name: "models target",
+      catchPhrase:
+        "Other men said they have seen angels, but I have seen thee. And thou art enough. ",
+      bs: "target end-to-end models"
+    }
   }
 ];
 
 
 export default users ;
+
+
+
