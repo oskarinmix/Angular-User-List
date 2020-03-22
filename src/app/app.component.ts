4dd04ffe36ebe81a8042599264b3f3845ffbdd96
Oscar Alberto Ceballos Contreras <oscaralbertoceballos@gmail.com>
2020-03-22 01:21:11 -0400

    app completada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import  users from './users';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'angularprojectslistapersonas';
+export class AppComponent implements OnInit {
+  title = "Labs XD Innovation Test";
+  users = null;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.users = users;
+    }, 2000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SpinnerComponent } from './spinner/spinner.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpinnerComponent,
+
   ],
   imports: [
     BrowserModule,
